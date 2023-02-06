9aa8d30916d72fe8ab5523c9c50327243c7666c1
wintttr <85644131+wintttr@users.noreply.github.com>
2023-02-06 16:47:30 +0300

    Провёл небольшой рефакторинг. Не факт, что правильный

--- LR1/Task 2.rb
+++ LR1/Task 2.rb
@@ -16,22 +16,26 @@ def coprime_count(x)
     count
 end
 
+# Находим сумму значений функции func от каждой цифры
+# числа x, удовлетворяющей предикату pred 
+def num_proc(x, pred, func)
+    acc = 0
+    while x > 0 do
+        if pred.call x % 10 then
+            acc += func.call x % 10
+        end
+        x = x / 10
+    end
+    acc
+end
 
 # СПРОСИТЬ ПРО ЭТО
 # Находим сумму цифр числа x, если pred(цифра) == true 
 def num_sum(x, pred)
-    sum = 0
-    while x > 0 do
-        if pred.call x % 10 then
-            sum += x % 10
-        end
-        x = x / 10
-    end
-    sum
+    num_proc x, pred, lambda { |_| 1 }
 end
 
 def num_div_by_3_sum(x)
     # И ПРО ЭТО
-    num_sum(x, lambda {|y| y % 3 == 0})
+    num_sum x, lambda {|y| y % 3 == 0}
 end
-
