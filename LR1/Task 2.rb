cfeae9a58c943f953b8f511ae69450f2fb712428
wintttr <85644131+wintttr@users.noreply.github.com>
2023-02-07 01:20:35 +0300

    Попытался сделать рефакторинг, выглядит страшно, не факт что работает

--- LR1/Task 2.rb
+++ LR1/Task 2.rb
@@ -5,38 +5,25 @@ end
 
 # Находим количество чисел, взаимнопростых с данным
 def coprime_count(x)
-    count = 0
-
-    for i in (2...x)
-        if coprime? x, i then 
-            count += 1 
-        end
-    end
-
-    count
+    (1..x).select{|i| coprime? x, i }.size
 end
 
 # Находим сумму значений функции func от каждой цифры
 # числа x, удовлетворяющей предикату pred 
 def num_proc(x, pred, func)
-    acc = 0
-    while x > 0 do
-        if pred.call x % 10 then
-            acc += func.call x % 10
-        end
-        x = x / 10
-    end
-    acc
+    x.digits.select {|i| pred.call i}
+            .map {|i| func.call i}
+            .sum
 end
 
-# СПРОСИТЬ ПРО ЭТО
+# СПРОСИТЬ ПРО ЭТО (можно ли сделать ограничение на передачу ТОЛЬКО предиката)
 # Находим сумму цифр числа x, удовлетворяющих предикату pred 
 def num_sum(x, pred)
     num_proc x, pred, lambda { |x| x }
 end
 
 def num_div_by_3_sum(x)
-    # И ПРО ЭТО
+    # И ПРО ЭТО (можно ли сделать это как-то лучше)
     num_sum x, lambda {|y| y % 3 == 0}
 end
 
@@ -47,30 +34,23 @@ end
 
 # Находим количество цифр числа x, взаимнопростых с y 
 def num_coprime_with_y_count(x, y)
+    # И ПРО ЭТО (можно ли это сделать без лямбды)
     num_count x, lambda {|t| coprime? t, y}
 end
 
 # Находим все делители числа x
 def all_divs(x)
-    arr = Array.new
-    for i in (2..x)
-        if x % i == 0 then
-            arr.push i
-        end
-    end
-
-    arr
+    array_of_divs = (2..x).select {|i| x % i == 0}
 end
 
 # Находим делитель числа, являющийся взаимно простым с
 # наибольшим количеством цифр данного числа.
 def find_max_div(x)
     divs = all_divs x
-    count_array = Array.new 
-    
-    divs.each { |i|
-        count_array.push num_coprime_with_y_count x, i
-    }
+
+    count_array = divs.map { |i| num_coprime_with_y_count x, i }
 
     divs[count_array.index count_array.max]
 end
+
+puts num_div_by_3_sum 261434
