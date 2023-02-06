2d7c00ad7f0ac09a3135f96d4b396536d8b85990
wintttr <85644131+wintttr@users.noreply.github.com>
2023-02-06 17:15:54 +0300

    Третий метод 2 задания

--- LR1/Task 2.rb
+++ LR1/Task 2.rb
@@ -30,12 +30,47 @@ def num_proc(x, pred, func)
 end
 
 # СПРОСИТЬ ПРО ЭТО
-# Находим сумму цифр числа x, если pred(цифра) == true 
+# Находим сумму цифр числа x, удовлетворяющих предикату pred 
 def num_sum(x, pred)
-    num_proc x, pred, lambda { |_| 1 }
+    num_proc x, pred, lambda { |x| x }
 end
 
 def num_div_by_3_sum(x)
     # И ПРО ЭТО
     num_sum x, lambda {|y| y % 3 == 0}
+end
+
+# Находим количество цифр числа x, удовлетворяющих предикату pred
+def num_count(x, pred)
+    num_proc x, pred, lambda {|_| 1}
+end
+
+# Находим количество цифр числа x, взаимнопростых с y 
+def num_coprime_with_y_count(x, y)
+    num_count x, lambda {|t| coprime? t, y}
+end
+
+# Находим все делители числа x
+def all_divs(x)
+    arr = Array.new
+    for i in (2..x)
+        if x % i == 0 then
+            arr.push i
+        end
+    end
+
+    arr
+end
+
+# Находим делитель числа, являющийся взаимно простым с
+# наибольшим количеством цифр данного числа.
+def find_max_div(x)
+    divs = all_divs x
+    count_array = Array.new 
+    
+    divs.each { |i|
+        count_array.push num_coprime_with_y_count x, i
+    }
+
+    divs[count_array.index count_array.max]
 end
