b042a576c86038fc6002f9b23b6906800e06ee7a
wintttr <85644131+wintttr@users.noreply.github.com>
2023-02-07 03:06:02 +0300

    Третья задачка 4 задания

--- LR1/Task 4.rb
+++ LR1/Task 4.rb
@@ -6,3 +6,10 @@ def count_in_interval(array, a, b)
     array.count {|i| i >= a && i <= b}
 end
 
+def count_between_min(array)
+    min1 = array.index array.min
+    array.delete_at min1
+    min2 = array.index array.min
+
+    min2 - min1 + 1
+end
