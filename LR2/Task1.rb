7350d70a0b06b03ca05f2d7f1d3bf1bd3b2edd9a
wintttr <85644131+wintttr@users.noreply.github.com>
2023-02-18 11:41:43 +0300

    Чёт изменил, нравится уже больше

--- LR2/Task1.rb
+++ LR2/Task1.rb
@@ -3,7 +3,7 @@ class Student
     attr_accessor :surname, :first_name, :mid_name
     attr_accessor :phone, :telegram, :mail, :git
 
-    def initialize(surname, first_name, mid_name, id=nil, phone=nil, telegram=nil, mail=nil, git=nil)
+    def initialize(surname, first_name, mid_name, id:nil, phone:nil, telegram:nil, mail:nil, git:nil)
         self.id = id
         self.surname = surname
         self.first_name = first_name
