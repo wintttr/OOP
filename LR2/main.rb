6a4c34340bba16081a67bd274cf01a8d7fee3e88
wintttr <85644131+wintttr@users.noreply.github.com>
2023-02-23 22:04:56 +0300

    Поменял программу, вызывающую класс Student

---
+++ LR2/main.rb
@@ -0,0 +1,10 @@
+require_relative "student"
+
+a = Student.new surname: "Курбатский", first_name: "Владимир", mid_name: "Александрович", git: "SeemerGG"
+b = Student.new surname: "Мищенко", first_name: "Александр", mid_name: "Николаевич", id: 1
+c = Student.new surname: "Якухнов", first_name:"Роман", mid_name: "Андреевич", telegram: "@redmag"
+
+
+puts a, $/
+puts b, $/
+puts c, $/
