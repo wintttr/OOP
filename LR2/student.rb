c7e014247a183be094f6c3825d95fd0de385101a
wintttr <85644131+wintttr@users.noreply.github.com>
2023-02-23 22:13:58 +0300

    Сделал методы join_with_comma и field_string методами класса Student Всё равно полями не пользуются, зачем им быть методами экземпляра. Не знаю ещё, зачем их вообще в классе Student держать. Наверное, надо их вынести в другой класс, потом..

--- LR2/student.rb
+++ LR2/student.rb
@@ -16,30 +16,30 @@ class Student
 
 	# Соединяет строки из массива запятой, пропуская элементы содержащие nil
 	# Если итоговая строка пустая, возвращает nil
-	def join_with_comma str_arr
+	def Student.join_with_comma str_arr
 		result = str_arr.compact.join(", ")
 		result.empty? ? nil : result  
 	end
 
-	def field_string prompt, field
+	def Student.field_string prompt, field
 		if field != nil then
 			"#{prompt}: #{field}"
 		end
 	end
 
 	def to_s
-		id = field_string "Ид", self.id
-		surname = field_string "Фамилия", self.surname
-		first_name = field_string "Имя", self.first_name
-		mid_name = field_string "Отчество", self.mid_name
-		phone = field_string "Телефон", self.phone
-		telegram = field_string "Телеграм", self.telegram
-		mail = field_string "Мейл", self.mail
-		git = field_string "Гит", self.git
+		id = Student.field_string "Ид", self.id
+		surname = Student.field_string "Фамилия", self.surname
+		first_name = Student.field_string "Имя", self.first_name
+		mid_name = Student.field_string "Отчество", self.mid_name
+		phone = Student.field_string "Телефон", self.phone
+		telegram = Student.field_string "Телеграм", self.telegram
+		mail = Student.field_string "Мейл", self.mail
+		git = Student.field_string "Гит", self.git
 		
-		full_name = join_with_comma [surname, first_name, mid_name]
-		phone_and_tg = join_with_comma [phone, telegram]
-		mail_and_git = join_with_comma [mail, git]
+		full_name = Student.join_with_comma [surname, first_name, mid_name]
+		phone_and_tg = Student.join_with_comma [phone, telegram]
+		mail_and_git = Student.join_with_comma [mail, git]
 
 		[id, full_name, phone_and_tg, mail_and_git].compact.join "\n"
 	end
