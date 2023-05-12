efd06de63c427ce95994061bdcff2400f210c308
wintttr <85644131+wintttr@users.noreply.github.com>
2023-05-12 17:01:25 +0300

    Исправил класс DBSingleton, добавил переподключение к бд

--- LR2/sql/db_singleton.rb
+++ LR2/sql/db_singleton.rb
@@ -3,16 +3,32 @@ require "mysql2"
 class DBSingleton
 	private_class_method :new
 
-	attr_accessor :db_client
-	private :"db_client="
-	
-	def initialize
-		self.db_client = Mysql2::Client.new(host: "localhost", 
-											username: "wintttr", 
-											database: "studentsdb")
-	end
-
 	@instance = new
+
+	def db_client= value
+		@db_client = value
+	end
+	
+	private :"db_client="
+
+	
+	def db_client
+		self.reload
+		
+		@db_client
+	end
+	
+	def reload
+		if @db_client.nil? or @db_client.closed? then
+			begin
+				self.db_client = Mysql2::Client.new(host: "localhost", 
+												username: "wintttr", 
+												database: "studentsdb")
+			rescue
+				self.db_client = nil
+			end
+		end
+	end
 	
 	def self.instance
 		@instance
