94bfe4e00e961a6794af55b2a02f7c29c518df38
wintttr <85644131+wintttr@users.noreply.github.com>
2023-05-12 09:26:37 +0300

    Что-то сделал с контроллером (не помню)

--- LR2/gui/student_list_controller.rb
+++ LR2/gui/student_list_controller.rb
@@ -1,6 +1,7 @@
 require "student_list_view.rb"
 require "students_list.rb"
 require "json_reader_writer.rb"
+require "db_singleton"
 
 class StudentListController
 	attr_accessor :view, :student_list, :list, :dlss
@@ -9,7 +10,7 @@ class StudentListController
 		self.view = view
 		self.list = StudentsList.new
 		
-		self.list.read_all_objects JSONReaderWriter.new "json_for_test.json"
+		self.list.read_all_objects DBReaderWriter.new "students", DBSingleton.instance
 		
 		self.dlss = self.list.get_k_n_student_short_list 0, self.view.class.table_row_count
 		self.dlss.view = self.view
